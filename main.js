9323d4f255b493a1fe7459e1a412340736b20020
Samuel Felipe <samuelfelipef@gmail.com>
2023-02-01 19:52:46 -0300

    melhorias

--- main.js
+++ main.js
@@ -1,10 +1,60 @@
+
+// Adiciona tarefa ao apertar Enter
+document.addEventListener('keyup', (event) => {
+    const addTaskButton = document.querySelector("#add-task")
+    if (event.keyCode === 13) {
+        addTaskButton.click()
+    }
+})
+
+const list = document.querySelector("#list")
+const cleanListButton = document.querySelector("#clear-list")
+cleanListButton.addEventListener('click', clearList)
+
 function addNewTask() {
-    let list = document.getElementById("list");
-    let text = document.getElementById("task-name").value;
+    // Pega texto e valida se está preenchido
+    let text = document.getElementById("task-name").value;   
+    if (text.length === 0) {
+        alert("Insira uma descrição para a tarefa")
+        return
+    } 
+
+    // Cria elemento li e adiciona atributos necessários
     let listItem = document.createElement("li");
     listItem.className = "list-item";
+    listItem.id = listLength() + 1
+    listItem.innerHTML = text
 
-    const textElement = document.createTextNode(text);
-    list.appendChild(textElement);
+    // Adiciona botão para apagar tarega junto com li recém criado.
+    listItem.appendChild(createDeleteButton(listLength() + 1))
+
+    // Adiciona li recém criado à lista.
     list.appendChild(listItem);
+    clearInputField()
+}
+
+function clearInputField() {
+    let input = document.querySelector("#task-name")
+    input.value = ''
+}
+
+function listLength() {
+    return list.children.length
+}
+
+function createDeleteButton() {
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = 'Apagar tarefa'
+    deleteButton.className = 'delete-task-button'
+    deleteButton.addEventListener('click', deleteItem)
+    
+    return deleteButton
+}
+
+function deleteItem(event) {
+    list.removeChild(event.path[1])
+}
+
+function clearList() {
+    list.innerHTML = ''
 }
